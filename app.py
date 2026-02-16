3a905fb813c9d80eacb29a77025000a8af65a7a0
leomhuot <leomhout1234@gmail.com>
2026-02-16 14:48:33 +0700

    migrate DB

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+load_dotenv()
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
